1122427fc3e14f4da7b7cfa20263a0c360f2fd18
lucaseo <lucaseo0147@gmail.com>
2022-03-08 16:17:23 +0900

    update ingestion part.

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,10 @@
-import pandas as pd
-import numpy as np
 import os
 import json
+from typing import List
 from datetime import datetime
 
+import pandas as pd
+import numpy as np
 
 
 
@@ -13,14 +14,52 @@ with open('config.json','r') as f:
 
 input_folder_path = config['input_folder_path']
 output_folder_path = config['output_folder_path']
+ingestion_record_file_name = config['ingestion_record_file_name']
+ingestion_file_name = config['ingestion_file_name']
 
 
 
 #############Function for data ingestion
-def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+def read_tabular_file(dir_name: str, file_name: str):
+    return pd.read_csv(os.path.join(dir_name, file_name), sep=',', encoding='utf-8')
+
+def clean_dataframe(df: pd.DataFrame):
+    # filter out duplicates
+    df = df.drop_duplicates()
+    return df
+
+def save_dataframe(df, output_dir_name: str, file_name: str):
+    df.to_csv(os.path.join(output_dir_name, file_name), sep=',', encoding='utf-8')
+
+def write_ingestion_record(file_name: str, output_file_dir, output_file_name, output_file_length):
+
+    with open(os.path.join(output_file_dir, file_name), 'a') as f:
+        f.write("{datetime}\t{location}\t{filename}\t{length}\n".format(datetime=datetime.now(),
+                                                                      location=output_file_dir,
+                                                                      filename=output_file_name,
+                                                                      length=output_file_length))
+
+
+def merge_multiple_dataframe(input_folder_dir: str,
+                             output_folder_dir: str,
+                             output_file_name: str,
+                             record_file_name: str,
+                             data_cols: List[str] = ["corporation", "lastmonth_activity", "lastyear_activity", "number_of_employees", "exited"]):
+    # check for datasets, compile them together, and write to an output file
+
+    file_name_ls = os.listdir(input_folder_dir)
+
+    df_list = pd.DataFrame(columns=data_cols)
+    for file_name in file_name_ls:
+        df = read_tabular_file(input_folder_dir, file_name)
+        df_list = df_list.append(df)
+
+
+    df_list = clean_dataframe(df_list)
+    save_dataframe(df_list, output_folder_path, output_file_name)
+    write_ingestion_record(record_file_name, output_folder_dir, output_file_name, len(df_list))
 
 
 
 if __name__ == '__main__':
-    merge_multiple_dataframe()
+    merge_multiple_dataframe(input_folder_path, output_folder_path, ingestion_file_name, ingestion_record_file_name)
