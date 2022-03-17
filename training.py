cb3ad2665639037315effa75fec13d7928b3da59
lucaseo <lucaseo0147@gmail.com>
2022-03-17 19:00:45 +0900

    modify train process.

--- training.py
+++ training.py
@@ -12,17 +12,14 @@ import json
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
-dataset_csv_name = config['ingestion_file_name']
-trained_model_name = config['trained_model_name']
+dataset_csv_path = os.path.join(config['output_folder_path'], config['ingestion_file_name'])
+model_path = os.path.join(config['output_model_path'], config['trained_model_name'])
 
-def load_data(path: str, filename: str):
-    file_dir = os.path.join(path, filename)
-    return pd.read_csv(file_dir)
+def load_data(data_path: str):
+    return pd.read_csv(data_path, encoding='utf-8', sep=',')
 
 #################Function for training the model
-def train_model(train_X, train_y, model_path, model_name):
+def train_model(train_X, train_y, model_path):
     
     #use this logistic regression for training
     model = LogisticRegression(C=1.0,
@@ -45,10 +42,9 @@ def train_model(train_X, train_y, model_path, model_name):
     model.fit(train_X, train_y)
     
     #write the trained model to your workspace in a file called trainedmodel.pkl
-    pickle.dump(model, open(os.path.join(model_path, model_name), 'wb'))
+    pickle.dump(model, open(os.path.join(model_path), 'wb'))
 
 if __name__ == "__main__":
 
-    df = load_data(dataset_csv_path, dataset_csv_name)
-    train_model(df.drop(columns=['corporation', 'exited']), df['exited'],
-                model_path=model_path, model_name=trained_model_name)
+    df = load_data(dataset_csv_path)
+    train_model(df[config['feature_variable']], df[config['target_variable']], model_path=model_path)
